4a5098749b24cb80d7eef9bc00e0423bd19ad042
Nijas Ma <nijasbinabbas@gmail.com>
2024-05-01 13:33:30 +0530

    updated googl11e

--- middleware/auth.js
+++ middleware/auth.js
@@ -7,7 +7,7 @@ const GOOGLE_CLIENT_SECRET = "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"
 passport.use(new GoogleStrategy({
     clientID:     GOOGLE_CLIENT_ID,
     clientSecret: GOOGLE_CLIENT_SECRET,
-    callbackURL: "https://kickzone.online:3000/auth/google/callback",
+    callbackURL: "https://kickzone.online/auth/google/callback",
     passReqToCallback   : true
   },
   function(request, accessToken, refreshToken, profile, done) {
